7807429f3df18fa57af31cceee4eac17973cec7f
Minecraftienne <tiny99@free.fr>
2016-12-17 19:57:45 +0100

    Initial commit

---
+++ app/src/main/java/org/esiea/merkiled_haumey/diet/Onglet3Fragment.java
@@ -0,0 +1,18 @@
+package org.esiea.merkiled_haumey.diet;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Onglet3Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_onglet1, container, false);
+
+        return view;
+    }
+}
